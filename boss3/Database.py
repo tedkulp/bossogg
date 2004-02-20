02aa954f90d1f167842d3a23e5d613061347b160
soahc <soahc@1cb8d312-81c2-0310-9830-cdabd6cdd411>
2004-02-20 22:34:32 +0000

    * More changes to for thread stuff
    git-svn-id: file:///tmp/repos/boss3/trunk@519 1cb8d312-81c2-0310-9830-cdabd6cdd411

--- boss3/Database.py
+++ boss3/Database.py
@@ -68,7 +68,7 @@ class Database:
 			f=f.f_back
 			return os.path.basename(f.f_code.co_filename), f.f_lineno
 
-		def execute(self, SQL, *args):
+		def __execute(self, SQL, *args):
 			needlock=0
 
 			if len(SQL)>0 and SQL.split()[0].lower() in ["delete", "update", "insert"]:
@@ -80,11 +80,29 @@ class Database:
 			try:
 				sqlite.Cursor.execute(self, SQL, *args)
 			except:
-
 				log.exception("SQL ERROR")
 			if needlock and not self.nolock:
 				sql_lock.release()
 				log.debug("lock", "Release lock for database writes at (%s:%d)" % self.getcaller())
+
+		def execute(self, SQL, *args):
+			loopcnt = 0
+
+			while 1:
+				loopcnt = loopcnt + 1
+				try:
+					sqlite.Cursor.execute(self, SQL, *args)
+					break
+				except OperationalError:
+					if loopcnt >= 5:
+						log.error("Can't get time with the DB, It's still locked after 5 retries")
+						raise
+					log.debug("sql", "DB is locked, sleeping, then trying again")
+					time.sleep(0.5)
+					continue
+				except:
+					log.exception("SQL ERROR")
+					break
 
 	def _cursor(self):
 		self.conn._checkNotClosed("cursor")
