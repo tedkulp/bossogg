48077b30606d45172d6ca38bd14ed139ab58dda4
may1937 <may1937@1cb8d312-81c2-0310-9830-cdabd6cdd411>
2004-05-09 22:35:29 +0000

    Deleting the old bossao2 because I was being bad and not using svn properly
    git-svn-id: file:///tmp/repos/boss3/trunk@608 1cb8d312-81c2-0310-9830-cdabd6cdd411

--- boss3/bossao2/shout.h
+++
@@ -1,5 +0,0 @@
-
-shout_t *bossao_shout_init (PyObject *cfgparser);
-void shout_encode_chunk (song_s *song, unsigned char *buffer, int size);
-void shout_clear (song_s *song);
-int shout_encoder_init (song_s *song, PyObject *cfgparser);
