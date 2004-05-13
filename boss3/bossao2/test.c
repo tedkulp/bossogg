038e58aad8f51e569a3487e75706deb7f7c62431
may1937 <may1937@1cb8d312-81c2-0310-9830-cdabd6cdd411>
2004-05-13 07:02:32 +0000

    Commit some start/stop code to the test app. Almost ready to begin hooking up the interfaces...
    git-svn-id: file:///tmp/repos/boss3/trunk@621 1cb8d312-81c2-0310-9830-cdabd6cdd411

--- boss3/bossao2/test.c
+++ boss3/bossao2/test.c
@@ -23,33 +23,41 @@
 #import "bossao.h"
 #import "input_plugin.h"
 #import "output_plugin.h"
+#import "thbuf.h"
 
 thbuf_t *thbuf;
+GMutex *cons_pause_mutex;
+int prod_pos, cons_pos;
 
 #define THBUF_SIZE 256
 
 static gpointer producer (gpointer p)
 {
-   int pos = 0;
+   //int pos = 0;
    LOG ("producer thread started");
 
    while (1) {
       int size;
       gchar *chunk = input_play_chunk (NULL, &size);
-      if (chunk == NULL) {
-	 LOG ("got a null chunk, exiting");
-	 exit (0);
-      }
       //LOG ("producing");
       //output_plugin_write_chunk_all (NULL, chunk, size);
-      thbuf_produce (thbuf, chunk, size, pos);
+      //thbuf_produce (thbuf, chunk, size, pos);
+      thbuf_produce (thbuf, chunk, size, prod_pos);
+      if (chunk == NULL) {
+	 LOG ("got a null chunk, ending thread");
+	 g_thread_exit (0);
+      }
       //LOG ("done producing");
       /* uncomment next line to have the producer play audio (single-threaded) */
-      pos++;
-      if (pos >= THBUF_SIZE) {
+      //pos++;
+      prod_pos++;
+      //if (pos >= THBUF_SIZE) {
+      if (prod_pos >= THBUF_SIZE) {
 	 LOG ("producer thread wrapped");
-	 pos = 0;
+	 //pos = 0;
+	 prod_pos = 0;
       }
+      g_thread_yield ();
    }
       
    LOG ("producer thread done");
@@ -61,28 +69,39 @@ static gpointer consumer (gpointer p)
 {
    int size;
    void *data;
-   int pos = 0;
+   //int pos = 0;
    
    LOG ("consumer thread started");
 
    while (1) {
+      g_mutex_lock (cons_pause_mutex);
       gchar *chunk;
       //LOG ("consuming");
-      data = thbuf_consume (thbuf, &size, pos);
+      //data = thbuf_consume (thbuf, &size, pos);
+      data = thbuf_consume (thbuf, &size, cons_pos);
       //LOG ("done consuming");
       chunk = (gchar *)data;
+      if (chunk == NULL) {
+	 LOG ("got a null chunk, ending thread");
+	 g_thread_exit (0);
+      }
       /* comment this next line if you want the producer to play audio */
       //LOG ("out size is %d %d", size, BUF_SIZE);
       output_plugin_write_chunk_all (NULL, chunk, size);
       g_free (chunk);
-      pos++;
-      if (pos >= THBUF_SIZE) {
+      //pos++;
+      cons_pos++;
+      //if (pos >= THBUF_SIZE) {
+      if (cons_pos >= THBUF_SIZE) {
 	 LOG ("consumer thread wrapped");
-	 pos = 0;
+	 //pos = 0;
+	 cons_pos = 0;
       }
+      g_mutex_unlock (cons_pause_mutex);
+      g_thread_yield ();
    }
 
-   LOG ("consumer thread done, got to %d", pos);
+   LOG ("consumer thread done, got to %d", cons_pos);
 
    return NULL;
 }
@@ -93,12 +112,17 @@ int main (int argc, char *argv[])
    input_plugin_s *plugin;
    song_s *song;
 
-   if (argc != 2) {
-      printf ("Usage: %s <filename.ogg>\n", argv[0]);
+   if (argc != 4) {
+      printf ("Usage: %s <filename1.ogg> <filename2.ogg> <secs-to-sleep>\n", argv[0]);
       exit (-1);
    }
 
+   int secs_to_sleep = atoi (argv[3]);
+   printf ("Sleeping for %d seconds between operations\n", secs_to_sleep);
+
    bossao_thread_init ();
+
+   cons_pause_mutex = g_mutex_new ();
    
    input_plugin_open ("input_mp3.la");
    input_plugin_open ("input_ogg.la");
@@ -128,26 +152,34 @@ int main (int argc, char *argv[])
       exit (-6);
    }
 
-   while (1)
-      sleep (1);
+   /* test pausing */
+   sleep (secs_to_sleep);
+   LOG ("pausing");
+   g_mutex_lock (cons_pause_mutex);
+   sleep (secs_to_sleep);
+   LOG ("unpausing");
+   g_mutex_unlock (cons_pause_mutex);
 
-   LOG ("restarting?");
-   
-   sleep (2);
+   /* test stopping and loading a new file */
+   sleep (secs_to_sleep);
+   LOG ("clearing");
+   g_mutex_lock (cons_pause_mutex);
    thbuf_clear (thbuf);
-   LOG ("cleared");
-   sleep (2);
+   prod_pos = 0;
+   cons_pos = 0;
+   //g_thread_yield ();
+   LOG ("done clearing");
+   //sleep (secs_to_sleep);
+   //g_usleep (10);
+   LOG ("done sleeping");
+   LOG ("closing input");
    input_close (song);
-   input_open (song, "/home/adam/test.ogg");
-
-   LOG ("about to do it...");
-   
-   g_mutex_lock (thbuf->empty->mutex);
-   g_cond_broadcast (thbuf->empty->cond);
-   g_mutex_unlock (thbuf->empty->mutex);
-   g_mutex_lock (thbuf->full->mutex);
-   g_cond_broadcast (thbuf->full->cond);
-   g_mutex_unlock (thbuf->full->mutex);
+   LOG ("done closing");
+   plugin = input_plugin_find (argv[2]);
+   input_plugin_set (plugin);
+   input_open (song, argv[2]);
+   g_mutex_unlock (cons_pause_mutex);
+   //sleep (secs_to_sleep);
 
    LOG ("joining..");
    
@@ -161,59 +193,7 @@ int main (int argc, char *argv[])
    output_plugin_close_all ();
    
    thbuf_free (thbuf);
+   g_mutex_free (cons_pause_mutex);
    
    return 0;
-   
-   /* OLD STUFF
-  song_s *testsong = NULL;
-  int i = 0;
-
-  printf("Creating new struct\n");
-  testsong = bossao_new();
-  printf ("preparing to start\n");
-  bossao_start (testsong, NULL);
-  char *filename = (char *)malloc (sizeof ("/home/adam/bbking.flac"));
-  strcpy (filename, "/home/adam/test.mp3");
-  char *filename2 = strdup (filename);
-  printf ("filename is %s\n", filename);
-  printf ("filename has %p\n", filename);
-  printf ("playing first file\n");
-  bossao_play (testsong, filename);
-  printf ("Driver name is %s\n", bossao_driver_name (testsong));
-  
-  while (i < 3) {
-    usleep (1000000);
-    i++;
-  }
-
-  printf ("pausing\n");
-  bossao_pause (testsong);
-  sleep (2);
-  printf ("unpausing\n");
-  bossao_unpause (testsong);
-
-  while (i < 4) {
-    usleep (1000000);
-    i++;
-  }
-
-  bossao_stop (testsong);
-  printf ("stopped\n");
-  sleep (2);
-  printf ("readying the play: %s\n", filename2);
-  bossao_play (testsong, filename2);
-  sleep (4);
-	
-  bossao_shutdown (testsong);
-	
-  usleep (200000);
-	
-  printf ("closing\n");
-
-  printf("Deleting struct\n");
-  bossao_del(testsong);
-  free (filename);
-	
-  return 0;
-   */
 }
