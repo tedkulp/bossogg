3f87e0fd8fe9b1b27b9daafebf5c87bd0eb34a2f
may1937 <may1937@1cb8d312-81c2-0310-9830-cdabd6cdd411>
2004-05-14 00:11:56 +0000

    Don't play NULL chunks
    git-svn-id: file:///tmp/repos/boss3/trunk@638 1cb8d312-81c2-0310-9830-cdabd6cdd411

--- boss3/bossao2/test.c
+++ boss3/bossao2/test.c
@@ -95,7 +95,8 @@ static gpointer consumer (gpointer p)
       //}
       /* comment this next line if you want the producer to play audio */
       LOG ("out chunk is %d %p", size, chunk);
-      output_plugin_write_chunk_all (NULL, chunk, size);
+      if (chunk != NULL && size != 0)
+	 output_plugin_write_chunk_all (NULL, chunk, size);
       g_free (chunk);
       //pos++;
       cons_pos++;
